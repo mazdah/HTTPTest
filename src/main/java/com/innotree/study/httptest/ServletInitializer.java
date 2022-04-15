4e5af42e369597491cc7fa8b31ae2379b59b20e2
mazdah <woohj70@hanmail.net>
2022-04-15 10:03:40 +0900

    Initial commit

---
+++ src/main/java/com/innotree/study/httptest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.innotree.study.httptest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HttpTestApplication.class);
+    }
+
+}
